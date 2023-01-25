51b202d4939ee1607be946813221ee18be5bf637
Dallin Blanc <dallinb94@gmail.com>
2023-01-25 09:41:36 -0700

    fixed array

--- Mission3_Assignment/Program.cs
+++ Mission3_Assignment/Program.cs
@@ -6,50 +6,33 @@ namespace Mission3_Assignment
     {
         static void Main(string[] args)
         {
-            string choice1 = " ";
-            string[,] board = new string[,] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+            string choice1 = "";
+            string choice2 = ""
+            string[] board = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
             Console.WriteLine("Welcome to Tic Tac Toe!");
 
-            Console.WriteLine(board[0, 0]);
-            Console.WriteLine(board[0, 1]);
-            Console.WriteLine(board[0, 2]);
-            Console.WriteLine(board[1, 0]);
-            Console.WriteLine(board[1, 1]);
-            Console.WriteLine(board[1, 2]);
-            Console.WriteLine(board[2, 0]);
-            Console.WriteLine(board[2, 1]);
-            Console.WriteLine(board[2, 2]);
-
-
-
-
             //Player 1 goes, Player 2 goes, Loop.
-            Console.WriteLine("Where would player 1 like to place an X? ");
+            Console.WriteLine("Where would Player 1 like to place an X?");
             choice1 = Console.ReadLine();
-            for(int i=0; i<3; i++)
+            for (int i = 0; i < 9; i++)
             {
-                for(int j=0; j<3; j++)
+                if (board[i] == choice1)
                 {
-                    if (choice1 == board[i, j])
-                    {
-                        board[i, j] = "X";
-                    }
+                    board[i] = "X";
                 }
             }
 
-            Console.WriteLine(board[0, 0]);
-            Console.WriteLine(board[0, 1]);
-            Console.WriteLine(board[0, 2]);
-            Console.WriteLine(board[1, 0]);
-            Console.WriteLine(board[1, 1]);
-            Console.WriteLine(board[1, 2]);
-            Console.WriteLine(board[2, 0]);
-            Console.WriteLine(board[2, 1]);
-            Console.WriteLine(board[2, 2]);
-
-
-
+            // 
+            Console.WriteLine("Where would Player 2 like to place an O?");
+            choice2 = Console.ReadLine();
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == choice2)
+                {
+                    board[i] = "O";
+                }
+            }
         }
     }
 }
