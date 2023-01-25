e70b68d0165f82f5457cbe995a7cfe18cfed59f6
Dallin Blanc <dallinb94@gmail.com>
2023-01-25 10:44:46 -0700

    lots of changes, not checking for overwriting and ties not working

--- Mission3_Assignment/Program.cs
+++ Mission3_Assignment/Program.cs
@@ -6,33 +6,47 @@ namespace Mission3_Assignment
     {
         static void Main(string[] args)
         {
+            SupportingClass sc = new SupportingClass();
             string choice1 = "";
-            string choice2 = ""
+            string choice2 = "";
+            string winner = "n";
             string[] board = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
             Console.WriteLine("Welcome to Tic Tac Toe!");
 
-            //Player 1 goes, Player 2 goes, Loop.
-            Console.WriteLine("Where would Player 1 like to place an X?");
-            choice1 = Console.ReadLine();
-            for (int i = 0; i < 9; i++)
+            while (winner == "n") 
             {
-                if (board[i] == choice1)
-                {
-                    board[i] = "X";
-                }
-            }
+                sc.PrintBoard(board);
 
-            // 
-            Console.WriteLine("Where would Player 2 like to place an O?");
-            choice2 = Console.ReadLine();
-            for (int i = 0; i < 9; i++)
-            {
-                if (board[i] == choice2)
+                Console.WriteLine("Where would Player 1 like to place an X?");
+                choice1 = Console.ReadLine();
+
+
+                for (int i = 0; i < 9; i++)
                 {
-                    board[i] = "O";
+                    if (board[i] == choice1)
+                    {
+                        board[i] = "X";
+                    }
+                }
+
+                winner = sc.CheckForWinner(board);
+                if (winner == "n")
+                {
+                    sc.PrintBoard(board);
+                    Console.WriteLine("\n\nWhere would Player 2 like to place an O?");
+                    choice2 = Console.ReadLine();
+                    for (int i = 0; i < 9; i++)
+                    {
+                        if (board[i] == choice2)
+                        {
+                            board[i] = "O";
+                        }
+                    }
+                    winner = sc.CheckForWinner(board);
                 }
             }
+            sc.PrintBoard(board);
         }
     }
 }
---
+++ Mission3_Assignment/SupportingClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+
+
+namespace Mission3_Assignment
+{
+    public class SupportingClass
+    {
+        public void PrintBoard(string[] boardArray)
+        {
+            Console.WriteLine(boardArray[0] + " | " + boardArray[1] + " | " + boardArray[2]);
+            Console.WriteLine("---------");
+            Console.WriteLine(boardArray[3] + " | " + boardArray[4] + " | " + boardArray[5]);
+            Console.WriteLine("---------");
+            Console.WriteLine(boardArray[6] + " | " + boardArray[7] + " | " + boardArray[8]);
+        }
+
+        public string CheckForWinner(string[] boardArray)
+        {
+            string winner = "n";
+
+            // 1, 2, 3 horizontal top row
+            if (boardArray[0] == boardArray[1] && boardArray[1] == boardArray[2])
+            {
+                winner = boardArray[0];
+            }
+
+            else if (boardArray[3] == boardArray[4] && boardArray[4] == boardArray[5])
+            {
+                winner = boardArray[3];
+            }
+
+            else if (boardArray[6] == boardArray[7] && boardArray[7] == boardArray[8])
+            {
+                winner = boardArray[6];
+            }
+
+            else if (boardArray[0] == boardArray[3] && boardArray[3] == boardArray[6])
+            {
+                winner = boardArray[0];
+            }
+
+            else if (boardArray[1] == boardArray[4] && boardArray[4] == boardArray[7])
+            {
+                winner = boardArray[1];
+            }
+
+            else if (boardArray[2] == boardArray[5] && boardArray[5] == boardArray[8])
+            {
+                winner = boardArray[2];
+            }
+
+            else if (boardArray[0] == boardArray[4] && boardArray[4] == boardArray[8])
+            {
+                winner = boardArray[0];
+            }
+
+            else if (boardArray[2] == boardArray[4] && boardArray[4] == boardArray[6])
+            {
+                winner = boardArray[2];
+            }
+
+            else if (boardArray[0] != "1" && boardArray[1] != "2" && boardArray[2] != "3" && boardArray[3] != "4" && boardArray[4] != "5" && boardArray[5] != "6" && boardArray[6] != "7" && boardArray[7] != "8" && boardArray[8] != "9")
+            {
+                Console.WriteLine("It's a Tie!");
+            }
+
+            if (winner != "n")
+            {
+                Console.WriteLine("Player " + winner + " wins!");
+            }
+            return winner;
+        }
+    }
+}
